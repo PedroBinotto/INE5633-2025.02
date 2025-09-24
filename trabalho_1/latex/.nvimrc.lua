3703ae27332999a288b8514aa2d6302d08a15928
Pedro Binotto <pedrosantibinotto@gmail.com>
2025-09-24 19:51:06 -0300

    remove unnecessary lines

--- trabalho_1/latex/.nvimrc.lua
+++ trabalho_1/latex/.nvimrc.lua
@@ -1,1 +1,1 @@
-vim.g.LatexEntryPoint = "/home/pedro/Documents/ufsc/INE5633-2025.02/trabalhos/trabalho_1/latex/article.tex"
+vim.g.LatexEntryPoint = "/home/blockful/Documents/ufsc/INE5633-2025.02/trabalho_1/latex/article.tex"
